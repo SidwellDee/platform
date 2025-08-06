63fcbd4fa568d5cc279c75953840f4a03b320224
Seedwell Dlamini <seedwell.dlamini@thepalladiumgroup.com>
2025-08-06 10:08:18 +0000

    adding observation, observation tables and mappings

--- analytics-datastore-clickhouse/importer/config/clickhouseTables.js
+++ analytics-datastore-clickhouse/importer/config/clickhouseTables.js
@@ -41,16 +41,40 @@ const queries =
 			)
 			ENGINE = ReplicatedMergeTree('/clickhouse/tables/{cluster}/{shard}/{table}', '{replica}')
 			ORDER BY tuple(inserted_at);`,
-        `CREATE TABLE observation_example ON CLUSTER '{cluster}' (
-					id String,
-					version String NULL,
-					inserted_at DateTime DEFAULT now(),
-					last_updated Date NULL,
-					patientId String,
-					observationValue Double,
-				) 
-				ENGINE = ReplicatedMergeTree('/clickhouse/tables/{cluster}/{shard}/{table}', '{replica}')
-				ORDER BY tuple();`,
+		`CREATE TABLE Observation (
+				id 							    String,
+				version 					    String NULL,			  						
+				inserted_at 				    DateTime DEFAULT now(),					
+				last_updated 				    Date NULL,	
+				status 						    String,
+				category_system 			    String,
+				category_code 				    String,
+				code_system 				    String,
+				code_code 					    String,
+				code_display 				    String,
+				code_text 					    String,
+				subject_reference 			    String,
+				encounter_reference 		    String,
+				practitioner_reference 		    String,
+				value_type 				    	String, -- Type of the value (e.g., Quantity, CodeableConcept, etc.)
+				value_quantity_value 			Decimal(18,4) NULL,
+				value_quantity_unit 			String NULL,			
+				value_code             			String,
+    			value_display          			String,
+    			value_string           			String,
+				value_boolean 				    Boolean NULL,
+				value_integer 				    Integer NULL,				
+				value_range_low        			Decimal(18,4) NULL,
+    			value_range_high       			Decimal(18,4) NULL,
+				value_ratio_num        			Decimal(18,4) NULL,
+    			value_ratio_den        			Decimal(18,4) NULL, 
+				value_sampled_data 			    String,
+				value_time 					    String,
+				value_datetime 				    DateTime NULL,
+				value_period_start 			    DateTime NULL,
+				value_period_end 			    DateTime NULL,  
+			) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{cluster}/{shard}/{table}', '{replica}')
+			ORDER BY tuple(inserted_at);`
       ]
 	: [
         `CREATE TABLE Patient(
@@ -90,18 +114,93 @@ const queries =
 				hospitalization_discharge_disp_code String
 			)
 			ENGINE = MergeTree
-			ORDER BY (inserted_at);
+			ORDER BY tuple(inserted_at);
 			`,
-        `CREATE TABLE observation_example(
-					id String,
-					version String NULL,
-					inserted_at DateTime DEFAULT now(),
-					last_updated Date NULL,
-					patientId String,
-					observationValue Double,
-				) 
-				ENGINE=MergeTree
-				ORDER BY tuple();`,
+		`CREATE TABLE Observation (
+				id 							    String,
+				version 					    String NULL,			  						
+				inserted_at 				    DateTime DEFAULT now(),					
+				last_updated 				    Date NULL,	
+				status 						    String,
+				category_system 			    String,
+				category_code 				    String,
+				code_system 				    String,
+				code_code 					    String,
+				code_display 				    String,
+				code_text 					    String,
+				subject_reference 			    String,
+				encounter_reference 		    String,
+				practitioner_reference 		    String,
+				value_type 				    	String, -- Type of the value (e.g., Quantity, CodeableConcept, etc.)
+				value_quantity_value 			Decimal(18,4) NULL,
+				value_quantity_unit 			String NULL,			
+				value_code             			String,
+    			value_display          			String,
+    			value_string           			String,
+				value_boolean 				    Boolean NULL,
+				value_integer 				    Integer NULL,				
+				value_range_low        			Decimal(18,4) NULL,
+    			value_range_high       			Decimal(18,4) NULL,
+				value_ratio_num        			Decimal(18,4) NULL,
+    			value_ratio_den        			Decimal(18,4) NULL, 
+				value_sampled_data 			    String,
+				value_time 					    String,
+				value_datetime 				    DateTime NULL,
+				value_period_start 			    DateTime NULL,
+				value_period_end 			    DateTime NULL,    
+			) ENGINE = MergeTree
+			ORDER BY tuple(inserted_at);`,
+			`CREATE TABLE DiagnosticReport
+			(
+				id                      String, 
+				version 			    String NULL,			  						
+				inserted_at 			DateTime DEFAULT now(),					
+				last_updated 			Date NULL,	
+				status                  String,
+				category_code           String,
+				category_display        String,
+				code_code               String,
+				code_display            String,
+				subject_reference       String,
+				encounter_reference     String,
+				effective_datetime      DateTime NULL,
+				issued                  DateTime NULL,
+				performer_reference     String,
+				specimen_reference      String,
+				result_reference        String,
+				conclusion              String,
+				presented_form          String
+			)
+			ENGINE = MergeTree
+			ORDER BY (inserted_at);`,
+			`CREATE TABLE MedicationRequest (
+				id                                              String,
+				version 					                    String NULL,			  						
+				inserted_at 				                    DateTime DEFAULT now(),					
+				last_updated 				                    Date NULL,	
+				status                                          String,
+				intent                                          String,
+				medication_code                					String,
+				medication_display             					String,
+				subject_reference                               String,
+				encounter_reference                             String,
+				authored_on                                     DateTime NULL,
+				requester_reference                             String,
+				dosage_instruction_text                         String,
+				dosage_instruction_timing_repeat_frequency      UInt32 NULL,
+				dosage_instruction_timing_repeat_period         Float32 NULL,
+				dosage_instruction_timing_repeat_period_unit    String,
+				dosage_instruction_route_code                   String,
+				dosage_instruction_route_display                String,
+				dispense_request_quantity_value                 Float32 NULL,
+				dispense_request_quantity_unit                  String,
+				dispense_request_expected_supply_duration_value Float32 NULL,
+				dispense_request_expected_supply_duration_unit  String,
+				substitution_allowed                            Boolean NULL,
+				substitution_reason_code                        String,
+				substitution_reason_display                     String
+			) ENGINE = MergeTree()
+			ORDER BY inserted_at;`
       ];
 
 module.exports = queries;
